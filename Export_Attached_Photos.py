64d6243059db555ad182f4da76f04f5ee76f192c
Kelly Harris <67355213+kharris@users.noreply.github.com>
2021-03-09 16:46:25 -0500

    Update Export_Attached_Photos.py

--- Export_Attached_Photos.py
+++ Export_Attached_Photos.py
@@ -2,9 +2,11 @@ import arcpy
 from arcpy import da
 import os
 
+#Get the name of the feature class with attached photos and the location to save the photos as parameters
 inTable = arcpy.GetParameterAsText(0)
 fileLocation = arcpy.GetParameterAsText(1)
 
+#Loop through each feature in the feature class and save the attached photo in the specified file
 with da.SearchCursor(inTable, ['DATA', 'ATT_NAME', 'ATTACHMENTID']) as cursor:
     for item in cursor:
         attachment = item[0]
